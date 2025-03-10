e82ac52adc2890911fafa6e463367fe6cb651993
alihaidar2950 <alihaidar2950@gmail.com>
2025-03-10 01:07:53 -0400

    Restructure project components and update import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import RestaurantList from "./components/RestaurantList";
+import RestaurantList from "../components/RestaurantList";
 import Link from "next/link";
 
 export default function Home() {
